8f38bd7b42ded773d7400df1d7d3f0990a46c1b8
Chris Hapgood <cch1@hapgoods.com>
2016-05-21 17:43:16 -0400

    Update template

--- src/project_euler/pe-template.clj
+++ src/project_euler/pe-template.clj
@@ -1,11 +1,12 @@
 (ns project-euler.pe-template
   "Description from PE page"
   {:problem-page "https://projecteuler.net/problem=N"}
-  (:require [clojure.test :as test :refer [with-test is]]
+  (:require [project-euler.util :refer [logging-execution-time]]
+            [clojure.test :as test :refer [with-test is]]
             [clojure.tools.logging :as log]))
 
 (with-test
   (defn ^{:answer 72} main
     []
-    72)
+    (logging-execution-time 72))
   (is (= (-> #'main meta :answer) (main))))

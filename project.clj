03a04708f6f0de53eb3e83bef0e678935775ba9b
Chris Hapgood <cch1@hapgoods.com>
2016-05-22 10:16:58 -0400

    Make it possible to run tests from the leiningen test task

--- project.clj
+++ project.clj
@@ -9,4 +9,5 @@
                  [org.slf4j/slf4j-api "1.7.21"]
                  [org.slf4j/jcl-over-slf4j "1.7.13"]
                  [ch.qos.logback/logback-classic "1.1.3"]]
+  :test-paths ["src"]
   :jvm-opts ["-Xss2m"])
